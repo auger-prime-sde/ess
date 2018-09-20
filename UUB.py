144268f526a3d409577c212643bdaaf76ec37e89
Petr Tobiska <tobiska@fzu.cz>
2018-09-20 22:55:16 +0200

    UUB.py: removed UUBdisp+UUBmeas et. al.; gener_funcparams: return also aflags (propagated flags); UUBdaq: added q_resp for propagated flags

--- UUB.py
+++ UUB.py
@@ -6,31 +6,22 @@
 """
 
 import httplib
-import json
 import logging
-import pickle
 import re
-import select
 import socket
 import threading
 from datetime import datetime, timedelta
 from time import sleep
-from struct import pack, unpack
+from struct import unpack
 from struct import error as struct_error
-from binascii import hexlify
 from Queue import Empty
 from telnetlib import Telnet
 import numpy
 
-PORT = 80
+HTTPPORT = 80
 DATAPORT = 8888    # UDP port UUB send data to
 CTRLPORT = 8887    # UDP port UUB listen for commands
 LADDR = "192.168.31.254"  # IP address of the computer
-
-MSGLEN_DISP = 9    # length of message from dispatcher to meas
-MSGLEN_MEAS = 11   # length of message from meas to dispatcher
-TOUT_ACK = 1.0     # timeout for ack
-TOUT_DONE = 5.0    # timeout for done
 
 
 def uubnum2ip(uubnum):
@@ -47,40 +38,10 @@ def ip2uubnum(ip):
     return uubnum
 
 
-def hashObj(o):
-    """Hash serialized object to 8B string"""
-    return pack(">q", hash(pickle.dumps(o)))
-
-
-def gener_param_default(**kwargs):
-    """Measurement parameters generator: return provided kwargs as dict"""
-    yield kwargs
-    return
-
-
-def gener_voltage_ch2(default_voltage, default_ch2):
-    """Return generator of voltage & ch2.
-default_voltage - a default voltage to be returned if not provided
-                - list of floats
-default_ch2 - default values of ch2 (list of 'ON'/'OFF' or True/False)
-"""
-    def gener(voltage=None, ch2=None, **kwargs):
-        if voltage is None:
-            voltage = default_voltage
-        if ch2 is None:
-            ch2 = default_ch2
-        d = kwargs.copy()
-        for ch in ch2:
-            d['ch2'] = ch
-            for v in voltage:
-                d['voltage'] = v
-                yield d
-    return gener
-
-
 def gener_funcparams():
     """Return generators of AFG and item parameters
-return [(timer.name, functype, generator), ...]
+return [(timer.name, functype, generator, aflags), ...]
+  aflags - parameters to store in q_resp
 """
     def generP(**kwargs):
         """Generator for functype pulse
@@ -123,8 +84,10 @@ return afg_dict, item_dict"""
                         item_dict['voltage'] = v
                     yield afg_dict, item_dict
 
-    return (('meas.pulse', 'P', generP),
-            ('meas.freq', 'F', generF))
+    return (('meas.pulse', 'P', generP,
+             ('meas_pulse_point', 'db_pulse')),
+            ('meas.freq', 'F', generF,
+             ('meas_freq_point', 'db_freq')))
 
 
 def isLive(uub, timeout=0):
@@ -136,7 +99,7 @@ uub - UUBmeas or UUBtsc (must have ip and logger attributes)
     exptime = datetime.now() + timedelta(seconds=timeout)
     # uub.logger.debug('exptime = %s',
     #                  exptime.strftime("%Y-%m-%d %H:%M:%S,%f"))
-    addr = (uub.ip, PORT)
+    addr = (uub.ip, HTTPPORT)
     while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -245,7 +208,7 @@ q_resp - queue to send response
                 if 'test_point' in flags:
                     res['test_point'] = flags['test_point']
             self.logger.debug('Connecting UUB')
-            conn = httplib.HTTPConnection(self.ip, PORT)
+            conn = httplib.HTTPConnection(self.ip, HTTPPORT)
             try:
                 # read Zynq temperature
                 if 'meas.thp' in flags:
@@ -271,7 +234,7 @@ Return as 'ab-cd-ef-01-00-00' or None if UUB is not live"""
         if timeout is not None and not isLive(self, timeout):
             return None
         self.logger.debug('Reading UUB serial number')
-        conn = httplib.HTTPConnection(self.ip, PORT)
+        conn = httplib.HTTPConnection(self.ip, HTTPPORT)
         try:
             # self.logger.debug('sending conn.request')
             conn.request('GET', '/cgi-bin/getdata.cgi?action=slowc&arg1=-s')
@@ -325,236 +288,12 @@ return dictionary: sc<uubnum>_<variable>: value
         return res
 
 
-class UUBdisp(threading.Thread):
-    """UUB dispatcher"""
-    def __init__(self, timer, afg, ulisten, gener_param=gener_param_default):
-        """Constructor
-timer - instance of timer
-afg - instance of AFG
-ulisten - instance of UUBlistener
-gener_param - generator of measurement paramters
-"""
-        super(UUBdisp, self).__init__()
-        self.timer, self.afg, self.gener_param = timer, afg, gener_param
-        self.ulisten = ulisten
-        self.socks = []
-        self.socks2add = []
-        self.flags = {}  # current task paramterers
-        self.h = None    # hash of current task
-
-    def registerUUB(self, uubnum):
-        """Create a socket pair for UUB, store one socket in UUBdisp and
-return the second to UUBmeas.
-uubnum currently not stored
-"""
-        sock_disp, sock_meas = socket.socketpair(
-            socket.AF_UNIX, socket.SOCK_DGRAM)
-        self.socks2add.append(sock_disp)
-        return sock_meas
-
-    def setParams(self, **params):
-        """Perform operations with set-up (voltage, function generator etc.)
-according to params.
-params keys: voltage, ch2
-"""
-        logger = logging.getLogger('UUBdisp')
-        logger.debug('setParams %s', repr(params))
-        kw = {k: params[k] for k in ('voltage', 'ch2')
-              if k in params}
-        self.afg.setOn(**kw)
-
-    def clearParams(self):
-        """Move experiment to idle after all parameters"""
-        logger = logging.getLogger('UUBdisp')
-        logger.debug('clearParams')
-        self.afg.setOff()
-
-    def checkSocks(self, socks, tout, msg):
-        """Wait for msg on all socks or timeout
-socks - list of sockets to wait for message (will be modified)
-tout - timeout
-msg - expected message prefix
-"""
-        tend = datetime.now() + timedelta(seconds=tout)
-        socklist = []
-        while datetime.now() < tend and socks:
-            tout = (tend - datetime.now()).total_seconds()
-            rlist, wlist, xlist = select.select(socks, [], [], tout)
-            for sock in rlist:
-                resp = sock.recv(MSGLEN_MEAS)
-                if resp[:MSGLEN_DISP] == msg:
-                    socklist.append(sock)
-                    socks.remove(sock)
-        return socklist
-
-    def run(self):
-        logger = logging.getLogger('UUBdisp')
-        while True:
-            self.timer.evt.wait()
-            # move socks2add -> socks
-            while self.socks2add:
-                self.socks.append(self.socks2add.pop())
-            if self.timer.stop.is_set():
-                logger.info('Timer stopped, stopping UUB meas')
-                for s in self.socks:
-                    s.send('K' + '\xFF'*8)
-                return
-            if 'meas.point' not in self.timer.flags:
-                logger.debug('Ticker meas.point not in timer.flags, skipping')
-                continue
-            timestamp = self.timer.timestamp   # store info from timer
-            tflags = self.timer.flags['meas.point']
-            logger.debug('Event timestamp %s, flags: %s',
-                         datetime.strftime(timestamp, "%Y-%m-%d %H:%M:%S"),
-                         repr(tflags))
-            # copy other relevant flags
-            aflags = {flag: tflags[flag]
-                      for flag in ('db_point', 'set_temp', 'meas_point')
-                      if flag in tflags}
-            aflags['timestamp'] = timestamp
-            # run measurement for all parameters
-            for flags in self.gener_param(**tflags):
-                self.setParams(**flags)
-                flags.update(aflags)
-                h = hashObj(flags)
-                self.h, self.flags = h, flags
-                # send measurement request
-                msg = 'M' + h
-                for s in self.socks:
-                    s.send(msg)
-                socks2wait = self.checkSocks(self.socks[:], TOUT_ACK, 'A'+h)
-                self.checkSocks(socks2wait, TOUT_DONE, 'D'+h)
-                # send cancel request
-                msg = 'C' + h
-                for s in self.socks:
-                    s.send(msg)
-            self.clearParams()
-
-
-class UUBmeas(threading.Thread):
-    """Implementation of thread for UUB data readout"""
-    def __init__(self, uubnum, disp, q_dp):
-        """Constructor.
-uubnum - UUB number
-disp - an instance of UUBdisp (to get sock_meas & flags)
-q_dp - a queue for results
-"""
-        super(UUBmeas, self).__init__()
-        self.uubnum, self.disp, self.q_dp = uubnum, disp, q_dp
-        self.sock = disp.registerUUB(uubnum)
-        self.ip = uubnum2ip(uubnum)
-        self.logger = logging.getLogger('UUBm%04d' % uubnum)
-        self.logger.info('UUB meas created, IP %s.', self.ip)
-
-    def checkSock(self, timeout=None):
-        """Check if dgram arrived to sock
-return (None, None) nothing received or K not for us or wrong msg
-       ('K', None) to stop
-       ('M', flaghash) to start measurement
-       ('C', flaghash) to cancel measurement
-"""
-        rlist, wlist, xlist = select.select([self.sock], [], [], timeout)
-        if not rlist:
-            return None, None  # nothing received - timeout
-        msg = self.sock.recv(MSGLEN_DISP)
-        if len(msg) < MSGLEN_DISP:
-            self.logger.error('Incomplete msg <%s>', repr(msg))
-            return None, None
-        if msg[0] == 'K':   # stop if K is for us (=uubnum) or for all (FFFF)
-            uubnum = unpack(">H", msg[1:3])[0]
-            if uubnum in (self.uubnum, 0xFFFF):
-                self.logger.info('Kill received')
-                return 'K', None
-        elif msg[0] in ('M', 'C'):
-            return msg[0], msg[1:]
-        else:
-            self.logger.error('Wrong msg <%s>', repr(msg))
-            return None, None
-
-    def run(self):
-        while True:
-            # IDLE state, wait for message from dispatcher
-            cmd, flaghash = self.checkSock()
-            if cmd == 'K':
-                return
-            elif cmd == 'C':
-                self.logger.debug('Cancel %s', hexlify(flaghash))
-                continue
-            elif cmd != 'M':
-                self.logger.error('Msg type M expected')
-                continue
-            # transit to WORK state
-            flags = self.disp.flags.copy()
-            self.logger.debug('Meas %s', hexlify(flaghash))
-            self.logger.debug('flags %s', repr(flags))
-            # send Ack to dispatcher
-            self.sock.send('A' + flaghash)
-            self.logger.debug('Ack %s sent', hexlify(flaghash))
-            # send request
-            conn = httplib.HTTPConnection(self.ip, PORT)
-            try:
-                conn.request('GET', '/cgi-bin/getdata.cgi?action=scope')
-            except (httplib.CannotSendRequest, socket.error,
-                    AttributeError) as e:
-                self.logger.error('HTTP request failed, %s', e.__str__())
-                conn.close()
-            self.logger.debug('Request scope sent')
-            cmd, flaghash1 = self.checkSock(0)
-            if cmd is not None:
-                conn.close()
-                if cmd == 'K':
-                    self.logger.info('Kill received')
-                    break
-                elif cmd == 'C':
-                    self.logger.info('Canceling measurement %s',
-                                     hexlify(flaghash))
-                    if flaghash != flaghash1:
-                        self.logger.error('Unexpected flaghash %s',
-                                          hexlify(flaghash1))
-                    continue
-                elif cmd == 'M':
-                    self.logger.error('Unexpected measurement msg')
-                    continue
-            # wait for response
-            try:
-                resp = conn.getresponse()
-            except (httplib.CannotSendRequest, socket.error,
-                    AttributeError) as e:
-                self.logger.error('HTTP get response failed, %s', e.__str__())
-                conn.close()
-                continue
-            # send Done to dispatcher
-            self.sock.send('D' + flaghash)
-            self.logger.debug('Done %s sent', hexlify(flaghash))
-            conn.close()
-            self.logger.debug('Response %d received', resp.status)
-            # TO DO: retry for not OK response
-            if resp.status != httplib.OK:
-                continue
-            # process data and pass to data processor
-            labels = ['adc%d' % i for i in range(10)]
-            data = resp.read()
-            self.logger.debug('Response read')
-            # transform json data to list(2048) of lists(10)
-            try:
-                y = [[float(trec[label]) for label in labels]
-                     for trec in json.loads(data)]
-                self.logger.debug('JSON transformed to list')
-            except ValueError as e:
-                self.logger.error('Error parsing UUB data, %s', e.__str__())
-                continue
-            flags['yall'] = numpy.array(y, dtype='float32')
-            flags['uubnum'] = self.uubnum
-            self.q_dp.put(flags)
-            self.logger.debug('Data put to q_dp')
-
-
 class UUBdaq(threading.Thread):
     """Thread managing data acquisition from UUBs"""
+    TOUT_PREP = 0.2   # delay between afg setting and trigger in s
     TOUT_DAQ = 0.1    # timeout between trigger and UUBlisten cancel
-    TOUT_PREP = 0.1   # delay between afg setting and trigger
 
-    def __init__(self, timer, afg, ulisten, trigdelay,
+    def __init__(self, timer, afg, ulisten, trigdelay, q_resp,
                  gener_param=gener_funcparams()):
         """Constructor
 timer - instance of timer
@@ -565,10 +304,11 @@ gener_param - generator of measurement paramters (see gener_funcparams)
 """
         super(UUBdaq, self).__init__()
         self.timer, self.afg, self.ulisten = timer, afg, ulisten
-        self.trigdelay = trigdelay
+        self.trigdelay, self.q_resp = trigdelay, q_resp
         self.tnames = [rec[0] for rec in gener_param]  # timer names
         self.functypes = {rec[0]: rec[1] for rec in gener_param}
         self.geners = {rec[0]: rec[2] for rec in gener_param}
+        self.aflags = {rec[0]: rec[3] for rec in gener_param}
         self.ulisten = ulisten
         self.uubnums = set()
         self.uubnums2add = []
@@ -590,10 +330,12 @@ gener_param - generator of measurement paramters (see gener_funcparams)
                       for tname in self.tnames
                       if tname in self.timer.flags}
             # copy other relevant flags
-            aflags = {flag: tflags[flag]
-                      for flag in ('db_point', 'set_temp', 'meas_point')
-                      if flag in tflags}
-            aflags['timestamp'] = timestamp
+            aflags = {'timestamp': timestamp}
+            for tname, flags in tflags.iteritems():
+                aflags.update({key: flags[key] for key in self.aflags[tname]
+                               if key in flags})
+            if len(aflags) > 1:
+                self.q_resp.put(aflags)
 
             # update uubnums
             while self.uubnums2add:
@@ -605,12 +347,13 @@ gener_param - generator of measurement paramters (see gener_funcparams)
             for tname in self.tnames:
                 if tname not in tflags:
                     continue
-                logger.info('executing %s, flags %s', tname, repr(tflags))
+                logger.info('executing %s, flags %s',
+                            tname, repr(tflags[tname]))
                 self.trigdelay.delay = self.functypes[tname]
                 # run measurement for all parameters
                 for afg_dict, item_dict in self.geners[tname](**tflags[tname]):
-                    item_dict.update(aflags)
                     logger.debug("params %s", repr(item_dict))
+                    item_dict['timestamp'] = timestamp
                     self.afg.setParams(**afg_dict)
                     self.afg.switchOn(True)
                     self.ulisten.done.clear()
