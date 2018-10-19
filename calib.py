362ffd902cce7345d347814bc2aef83ca4077869
Petr Tobiska <tobiska@fzu.cz>
2018-10-19 16:51:49 +0200

    new calib.py & mdo.py

---
+++ calib.py
@@ -0,0 +1,171 @@
+"""
+  ESS procedure
+  Calibrate AFG/cabels/splitter path
+"""
+
+import sys
+import os
+import json
+import logging
+from datetime import datetime
+from time import sleep
+import numpy as np
+# ESS stuff
+from afg import AFG, splitter_amplification
+from mdo import MDO
+from UUB import gener_funcparams
+from dataproc import item2label, label2item, float2expo
+from hsfitter import SineFitter
+
+try:
+    with open(sys.argv[1], 'r') as fp:
+        d = json.load(fp)
+except (IndexError, IOError, ValueError):
+    print("Usage: %s <JSON config file>" % sys.argv[0])
+    raise
+
+# timeouts
+TOUT_PREP = 0.2   # delay between afg setting and trigger in s
+TOUT_DAQ = 0.1    # timeout between trigger and oscilloscope readout
+
+datadir = datetime.now().strftime(d.get('datadir', './'))
+if not os.path.isdir(datadir):
+    os.mkdir(datadir)
+
+if 'logging' in d:
+    kwargs = {key: d['logging'][key]
+              for key in ('level', 'format', 'filename')
+              if key in d['logging']}
+    if 'filename' in kwargs:
+        kwargs['filename'] = datetime.now().strftime(kwargs['filename'])
+    logging.basicConfig(**kwargs)
+logger = logging.getLogger('calib')
+
+afg = AFG(tmcid=d['usbtmc']['afg'], functype='F')
+mdo = MDO(tmcid=d['usbtmc']['mdo'])
+for item in d['setup']:
+    mdo.send(item)
+
+generF = [rec[2] for rec in gener_funcparams() if rec[1] == 'F'][0]
+afgkwargs = d.get('afgkwargs', {})
+
+dataslice = d['dataslice'] if 'dataslice' in d else None
+
+# oscilloscope channels with signals
+cho = d.get('cho')
+ch9 = d.get('ch9')
+
+# sampling frequency and number of datapoints after dataslice
+mdo.send('HEADER 0')
+resp = mdo.send('HORIZONTAL:SCALE?', resplen=100)
+horscale = float(resp.rstrip())
+resp = mdo.send('HORIZONTAL:RECORDLENGTH?', resplen=100)
+N = int(resp.rstrip())
+FREQ = 1e-6 * N/10 / horscale  # in MHz
+if dataslice is not None:
+    start, stop, step = dataslice
+    if start is None:
+        start = 0
+    if stop is None:
+        stop = N
+    if step is None:
+        step = 1
+    N = (stop - start)/step
+    FREQ /= step
+logger.debug('N = %d, FREQ = %f, dataslice: %d:%d:%d',
+             N, FREQ, *dataslice)
+nharm = d.get('nharm', 1)
+sf = SineFitter(N=N, FREQ=FREQ, NHARM=nharm)
+sf.crop = False
+
+datalog = open(datadir + 'datalog.txt', 'w')
+prolog = """\
+# calibration of splitter: trace fit results
+# columns: flabel | freq[MHz] | ch2 | input voltage[V] |
+#          ampli_o[V] | ampli_9[V] | chi_o[V] | chi_9[V] |
+#          cos/sin coeffs[V]: n*omega, n = 1..%d
+#          vandermont coeffs: x**k, k = 0..%d
+#              x = (2*t + 1)/N - 1, t = 0..N - timebin
+""" % (sf.NHARM, sf.NPOLY)
+datalog.write(prolog)
+dlog_formstr = ("%-4s %5.2f  %d %4.2f" +
+                "   %7.5f %7.5f   %7.5f %7.5f" +
+                "  %8.5f"*(2*sf.NHARM + 1 + sf.NPOLY) + "  "
+                "  %8.5f"*(2*sf.NHARM + 1 + sf.NPOLY) + "\n")
+
+fitlog = open(datadir + 'fitlog.txt', 'w')
+prolog = """\
+# calibration of splitter: linearity fit
+# columns: flabel | freq[MHz] | ch2 | senso | sens9 | 1-rho_o | 1-rho_9
+"""
+fitlog.write(prolog)
+fit_formstr = "%-4s %5.2f  %d   %7.5f %7.5f  %7.5f %7.5f\n"
+
+# run measurement for all parameters
+calibdata = {}
+afg.switchOn(True)
+for afg_dict, item_dict in generF(**afgkwargs):
+    logger.debug("params %s", repr(item_dict))
+    afg.setParams(**afg_dict)
+    # oscilloscope y-scale
+    voltage = afg.param['Fvoltage']
+    ch2 = afg.param['ch2']
+    freq = afg.param['freq']
+    flabel = float2expo(freq)
+    for ch_mdo, chan_uub in ((cho, 1), (ch9, 9)):
+        scale = 2 * voltage * splitter_amplification(ch2, chan_uub) / 6.0
+        mdo.send('CH%d:SCALE %f' % (ch_mdo, scale))
+    mdo.send('ACQUIRE:STATE ON')
+    sleep(TOUT_PREP)
+    afg.trigger()
+    logger.debug('trigger sent')
+    sleep(TOUT_DAQ)
+    vlabel = item2label(functype='F', ch2=ch2, voltage=voltage, freq=freq)
+    label = item2label(functype='F', ch2=ch2, freq=freq)
+    if label not in calibdata:
+        calibdata[label] = []
+    yall = np.zeros((N, 0))
+    for ch_mdo, typ in ((cho, 'datao'), (ch9, 'data9')):
+        res = mdo.readWFM(ch_mdo, dataslice)
+        fname = datadir + typ + '_' + vlabel + '.txt'
+        np.savetxt(fname, res[0])
+        yall = np.append(yall, res[0].reshape((N, 1)), axis=1)
+    resd = sf.fit(yall, flabel, freq, SineFitter.CHI)
+    calibdata[label].append((voltage, resd['ampli'].tolist()))
+    datalog.write(dlog_formstr % tuple([flabel, freq/1e6, ch2, voltage] +
+                                       resd['ampli'].tolist() +
+                                       resd['chi'].tolist() +
+                                       resd['param'].flatten('C').tolist()))
+afg.switchOn(False)
+datalog.close()
+
+# calculate sensitivity fit calibdata vs. voltage
+calibration = {}
+for label, vallist in calibdata.iteritems():
+    item = label2item(label)
+    sampo = splitter_amplification(item['ch2'], 0)
+    samp9 = splitter_amplification(item['ch2'], 9)
+    # [[ v_i, ampo_i, amp9_i], ...]
+    xy = np.zeros((len(vallist), 3))
+    for i, (voltage, [ampo, amp9]) in enumerate(vallist):
+        xy[i, :] = voltage, ampo/sampo, amp9/samp9
+    # xx_xy = [v1, v2, ...] * xy
+    xx_xy = xy.T[0].dot(xy)
+    slopes = xx_xy[1:] / xx_xy[0]
+    # correlation coeff = cov(V, ADC) / sqrt(var(V) * var(ADC))
+    if xy.shape[0] > 1:
+        covm = np.cov(xy, rowvar=False)
+        coeffo = 1.0 - covm[0][1] / np.sqrt(covm[0][0] * covm[1][1])
+        coeff9 = 1.0 - covm[0][2] / np.sqrt(covm[0][0] * covm[2][2])
+    else:
+        coeffo, coeff9 = 0.0, 0.0
+    freq = item['freq']
+    flabel = float2expo(freq)
+    fitlog.write(fit_formstr % (flabel, freq/1.e6, item['ch2'],
+                                slopes[0], slopes[1], coeffo, coeff9))
+    calibration[label] = slopes.tolist()
+
+fitlog.close()
+
+with open(datadir + 'calib.json', 'w') as fp:
+    json.dump(calibration, fp, indent=2, sort_keys=True)
---
+++ mdo.py
@@ -0,0 +1,113 @@
+"""
+  Communication with MDO3000
+"""
+
+import logging
+import os
+import errno
+from struct import unpack
+import numpy as np
+
+
+class MDO(object):
+    """Class for data readout from oscilloscope MDO3000"""
+    PARAM = {'WFMOUTPRE:BYT_NR': 2,
+             'DATA:ENCDG': 'RIBINARY'}
+    STRPARAM = ('WFMOUTPRE:BYT_OR', 'WFMOUTPRE:BN_FMT', 'WFMOUTPRE:ENCDG',
+                'DATA:ENCDG')
+    NUMPARAM = ('WFMOUTPRE:BYT_NR', 'WFMOUTPRE:BIT_NR')
+    ENDIAN = {'MSB': '>', 'LSB': '<'}
+    NRTYPE = {'8': 'b', '16': 'h'}
+
+    def __init__(self, tmcid=2, **kwargs):
+        self.logger = logging.getLogger('MDO')
+        device = '/dev/usbtmc%d' % tmcid
+        self.fd = None
+        try:
+            self.fd = os.open(device, os.O_RDWR)
+            self.logger.debug('%s open', device)
+            os.write(self.fd, '*IDN?')
+            resp = os.read(self.fd, 1000)
+            self.logger.info('Connected, %s', resp)
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                self.logger.error('Device %s does not exist (ENOENT)', device)
+            elif e.errno == errno.EACCES:
+                self.logger.error('Access to %s denied (EACCESS)', device)
+            else:
+                self.logger.error('Error opening %s - %s, %s',
+                                  device, errno.errorcode[e.errno], e.args[1])
+            if self.fd is not None:
+                os.close(self.fd)
+            raise
+        self.setParams(**MDO.PARAM)
+        params = {key: kwargs[key] for key in MDO.NUMPARAM + MDO.STRPARAM
+                  if key in kwargs}
+        self.setParams(**params)
+
+    def __del__(self):
+        self.logger.info('closing')
+        os.close(self.fd)
+
+    def send(self, line, lvl=logging.DEBUG, resplen=0):
+        """Send line to MDO3000
+lvl - optional logging level
+"""
+        line.rstrip()
+        self.logger.log(lvl, 'Sending %s', line)
+        os.write(self.fd, line)
+        if resplen > 0:
+            resp = os.read(self.fd, resplen)
+            return resp
+
+    def setParams(self, **d):
+        """Set MDO parameters according to dict <d>"""
+        for key, val in d.iteritems():
+            if key in MDO.NUMPARAM:
+                self.send('%s %d' % (key, val))
+            elif key in MDO.STRPARAM:
+                self.send('%s %s' % (key, val))
+
+    def _parseWFM(self, resp):
+        """Parse response to :WFMOUTPRE?"""
+        d = dict([item.split(' ', 1)
+                  for item in resp.split(';')])
+        assert d['ENCDG'] == 'BINARY'
+        assert d['BN_FMT'] == 'RI'
+        return {key: val for key, val in d.iteritems()
+                if key in ('BYT_OR', 'BIT_NR',
+                           'XUNIT', 'XZERO', 'XINCR',
+                           'YUNIT', 'YZERO', 'YMULT', 'YOFF')}
+
+    def readWFM(self, ch, dataslice=None):
+        """Read waveform from oscilloscope
+ch - oscilloscope channel to read
+dataslice - optional (start, stop, step) to slice acquired data
+return tuple (numpy.array yvals, float xincr, float xzero, xunit, yunit)"""
+        self.send('DATA:SOURCE CH%d' % ch)
+        self.send('HEADER 1')
+        self.send('WFMOUTPRE?')
+        resp = os.read(self.fd, 1000).rstrip()
+        self.logger.debug('WFM: %s', resp)
+        wfmd = self._parseWFM(resp)
+        self.send('HEADER 0')
+        self.send('CURVE?')
+        h = os.read(self.fd, 2)
+        assert h[0] == '#'
+        numpt = int(os.read(self.fd, int(h[1])))
+        data = os.read(self.fd, numpt)
+        ndata = numpt/(int(wfmd['BIT_NR'])/8)
+        fmtstr = (MDO.ENDIAN[wfmd['BYT_OR']] + str(ndata) +
+                  MDO.NRTYPE[wfmd['BIT_NR']])
+        yvals = np.array(unpack(fmtstr, data)) - float(wfmd['YOFF'])
+        yvals = float(wfmd['YZERO']) + float(wfmd['YMULT'])*yvals
+        xincr, xzero = map(lambda k: float(wfmd[k]), ('XINCR', 'XZERO'))
+        xunit, yunit = map(lambda k: wfmd[k].strip('"'), ('XUNIT', 'YUNIT'))
+        if dataslice is not None:
+            start, stop, step = dataslice
+            yvals = yvals[start:stop:step]
+            if start is not None:
+                xzero += xincr*start
+            if step is not None:
+                xincr *= step
+        return (yvals, xincr, xzero, xunit, yunit)
