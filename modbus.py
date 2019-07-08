57aed8f0bb4411614395b964f492378fad3b6abf
Petr Tobiska <tobiska@fzu.cz>
2019-07-08 10:04:42 +0200

    modbus.py: def. segment jump, PEP8 clean up, baudrate 38k

--- modbus.py
+++ modbus.py
@@ -4,20 +4,19 @@
 # Petr Tobiska <tobiska@fzu.cz>
 #
 
-VERSION = '20180104'
-
-import sys
-import time
 from struct import pack, unpack
-from binascii import hexlify, unhexlify
+from binascii import hexlify
 import logging
 import serial
 import crcmod
+
+VERSION = '20190707'
 
 # constants
 READ_HOLDING_REGISTERS = 0x03
 WRITE_SINGLE_REGISTER = 0x06
 WRITE_MULTIPLE_REGISTERS = 0x10
+
 
 class ModbusError(Exception):
     ERROR_INVALID_FUN     = 1
@@ -29,16 +28,18 @@ class ModbusError(Exception):
     def __init__(self, msg, code=None):
         super(ModbusError, self).__init__(msg)
         self.code = code
+
     def __str__(self):
         if self.code is not None:
             return ("Modbus error, code %d\n" % self.code)
         else:
             return super(ModbusError, self).__str__()
 
+
 class Modbus:
     """Implementation of Modbus client subset"""
-    def __init__(self, port, baudrate=9600, timeout=1, slave_id=1):
-        """Constructor 
+    def __init__(self, port, baudrate=38400, timeout=1, slave_id=1):
+        """Constructor
 port      - device to connect (/dev/ optional), str
 baudrate  - baudrate to use, int
 timeout   - timeout for serial read (seconds, float)
@@ -46,8 +47,6 @@ timeout   - timeout for serial read (seconds, float)
         self.slave_id = slave_id
         self.crc = crcmod.predefined.mkCrcFun('modbus')
         self.logger = logging.getLogger('modbus')
-        if not port.startswith('/dev/') and sys.platform == 'linux2':
-            port = '/dev/' + port
         try:
             self.ser = serial.Serial(port, baudrate, serial.EIGHTBITS,
                                      serial.PARITY_NONE, serial.STOPBITS_ONE,
@@ -106,7 +105,7 @@ return data with CRC stripped
         """Modbus function READ_HOLDING_REGISTERS (0x03)
 reg_addr     - register address (0 to 0xFFFF)
 reg_nb       - number of registers (1 to 125)
-""" 
+"""
         assert 0 <= reg_addr <= 0xFFFF, "reg_addr out of range"
         assert 1 <= reg_nb <= 80, "reg_nb out of range"
         frame = pack('>BBHH', self.slave_id, READ_HOLDING_REGISTERS,
@@ -120,7 +119,7 @@ reg_nb       - number of registers (1 to 125)
         """Modbus function WRITE_SINGLE_REGISTER (0x06)
 reg_addr     - register address (0 to 0xFFFF)
 reg_value    - value to write (0 to 0xFFFF)
-""" 
+"""
         assert 0 <= reg_addr <= 0xFFFF, "reg_addr out of range"
         assert 0 <= reg_value <= 0xFFFF, "reg_value out of range"
         frame = pack('>BBHH', self.slave_id, WRITE_SINGLE_REGISTER,
@@ -132,7 +131,7 @@ reg_value    - value to write (0 to 0xFFFF)
         """Modbus function WRITE_MULTIPLE_REGISTERS (0x10)
 reg_addr     - register address (0 to 0xFFFF)
 reg_values   - list of values to write (each 0 to 0xFFFF)
-""" 
+"""
         assert 0 <= reg_addr <= 0xFFFF, "reg_addr out of range"
         assert isinstance(reg_values, list), "reg_valus not a list"
         n = len(reg_values)
@@ -154,8 +153,10 @@ reg_values   - list of values to write (each 0 to 0xFFFF)
 
     def write_int(self, reg_addr, ival):
         """Convert int to 2 words and write them to <reg_addr>"""
-        self.write_multiple_registers(reg_addr, [ival / 0x10000, ival % 0x10000])
-        
+        self.write_multiple_registers(reg_addr,
+                                      [ival / 0x10000, ival % 0x10000])
+
+
 def words2floats(*words):
     """Convert list of 2*n words to list of n floats"""
     assert len(words) % 2 == 0, "Even number of words expected"
@@ -166,6 +167,7 @@ def words2floats(*words):
         vals.append(unpack(">f", blob)[0])
     return vals
 
+
 def floats2words(*floats):
     """Convert list of n floats to list of 2*n words"""
     vals = []
@@ -175,40 +177,44 @@ def floats2words(*floats):
         vals.extend([w2, w1])
     return vals
 
+
 class Binder:
     """Interface to Binder MKFT 115"""
-    # register addresses
+    # register addresses, MKFT 115 E.2, MB1 controller
     ADDR_ACT_TEMP      = 0x11A9
     ADDR_ACT_HUMID     = 0x11CD
-    ADDR_SET_TEMP      = 0x1177
-    ADDR_SET_HUMID     = 0x1179
-    ADDR_SET_TEMP_BAS  = 0x1581
-    ADDR_SET_HUMID_BAS = 0x1583
+    ADDR_SET_TEMP      = 0x1077
+    ADDR_SET_HUMID     = 0x1079
     ADDR_SET_TEMP_MAN  = 0x156F
     ADDR_SET_HUMID_MAN = 0x1571
+    ADDR_SET_TEMP_BAS  = 0x1581
+    ADDR_SET_HUMID_BAS = 0x1583
+    ADDR_PROG_RESET    = 0x1A00
+    ADDR_PROG_STATUS   = 0x1A01
     ADDR_PROG_NO       = 0x1A02            # program number
     ADDR_PROG_TYPE     = 0x1A03            # 0 .. temperature, 1 .. humidity
     ADDR_PROG_SEG      = 0x1A04            # segment programmed
     ADDR_PROG_NSEG     = 0x1A05            # total number of segments
-    ADDR_PROG_6        = 0x1A06            # ??? repetion ?
+    ADDR_PROG_6        = 0x1A06            # ???
     ADDR_PROG_VAL      = 0x1A07            # target value 
     ADDR_PROG_GRAD     = 0x1A09            # gradient ?
     ADDR_PROG_LIMI     = 0x1A0B            # limit min
     ADDR_PROG_LIMA     = 0x1A0D            # limit max
     ADDR_PROG_DUR      = 0x1A0F            # duration
     ADDR_PROG_OPERC    = 0x1A11            # operational contacts
-    ADDR_PROG_12       = 0x1A12            # ??? repetion ?
-    ADDR_PROG_13       = 0x1A13            # ??? repetion ?
+    ADDR_PROG_NUM_JUMP = 0x1A12            # number of jumps back
+    ADDR_PROG_SEG_JUMP = 0x1A13            # segment to jump back (from 0)
     ADDR_PROG_END      = 0x1599            # end of program
-    
+
     ADDR_MODE          = 0x1A22
     ADDR_PROGNO        = 0x1A23
     # constants
     STATE_BASIC =  0x1000
     STATE_MANUAL = 0x0800
     STATE_PROG =   0x0400
-    P_TEMP = 0
-    P_HUMID = 1
+    P_TEMP = 0                             # segment type temperature
+    P_HUMID = 1                            # segment type humidity
+    NPROG = 25                      # max. number of programs (numbered from 0)
 
     def __init__(self, modbus):
         assert isinstance(modbus, Modbus), "Modbus instance expected"
@@ -216,20 +222,20 @@ class Binder:
 
     def reset(self):
         """Some initialization"""
-        self.modbus.write_single_register(0x1A00, 5)
-        v1 = self.modbus.read_holding_registers(0x1A00)
-        v2 = self.modbus.read_holding_registers(0x1A01)
+        self.modbus.write_single_register(Binder.ADDR_PROG_RESET, 5)
+        self.modbus.read_holding_registers(Binder.ADDR_PROG_RESET)
+        self.modbus.read_holding_registers(Binder.ADDR_PROG_STATUS)
 
     def state(self):
         """Read state from chamber"""
-        return self.modbus.read_holding_registers(Binder.ADDR_MODE)
+        return self.modbus.read_holding_registers(Binder.ADDR_MODE)[0]
 
     def setState(self, state, progno=None):
         assert state in (Binder.STATE_BASIC, Binder.STATE_MANUAL,
                          Binder.STATE_PROG), "Incorrect Binder state"
         if state == Binder.STATE_PROG and progno is not None:
-            assert 0 <= progno <= 19, "Incorrect Prog No"
-        s = self.state()
+            assert 0 <= progno < Binder.NPROG, "Incorrect Prog No"
+        self.state()  # read current state
         self.modbus.write_single_register(Binder.ADDR_MODE, 0)
         if state == Binder.STATE_PROG and progno is not None:
             self.modbus.write_single_register(Binder.ADDR_PROGNO, progno)
@@ -241,6 +247,7 @@ class Binder:
     def getActHumid(self):
         return self.modbus.read_float(Binder.ADDR_ACT_HUMID)
 
+
 class BinderSegment:
     """Program segment"""
     def __init__(self, val, duration, **kw):
@@ -250,6 +257,9 @@ class BinderSegment:
         self.minlim = kw.get('minlim', -999.)
         self.maxlim = kw.get('maxlim', 999.)
         self.operc = kw.get('operc', 0)
+        self.r6 = kw.get('r6', 0)
+        self.numjump = kw.get('numjump', 0)
+        self.segjump = kw.get('segjump', 0)
 
 
 class BinderProg:
@@ -268,8 +278,11 @@ class BinderProg:
 
     def send(self, binder, progno):
         assert isinstance(binder, Binder), "Binder instance expected"
-        assert 0 <= progno <= 100, "Wrong Prog Number"
+        assert len(self.seg_temp) <= 100, "Max. 100 segments allowed"
+        assert len(self.seg_humid) <= 100, "Max. 100 segments allowed"
+        assert 0 <= progno < Binder.NPROG, "Incorrect Prog No"
         m = binder.modbus
+        # clear existing program TBD
         binder.reset()
         for segtype, segs in ((Binder.P_TEMP, self.seg_temp),
                               (Binder.P_HUMID, self.seg_humid)):
@@ -279,18 +292,15 @@ class BinderProg:
 
             for i, s in enumerate(iter(segs)):
                 m.write_single_register(Binder.ADDR_PROG_SEG, i)
-                m.write_single_register(Binder.ADDR_PROG_6, 0)
+                m.write_single_register(Binder.ADDR_PROG_6, s.r6)
                 m.write_float(Binder.ADDR_PROG_VAL, s.val)
                 m.write_float(Binder.ADDR_PROG_GRAD, s.grad)
                 m.write_int(Binder.ADDR_PROG_DUR, s.duration)
-                m.write_single_register(Binder.ADDR_PROG_12, 0)
-                m.write_single_register(Binder.ADDR_PROG_13, 0)
+                m.write_single_register(Binder.ADDR_PROG_NUM_JUMP, s.numjump)
+                m.write_single_register(Binder.ADDR_PROG_SEG_JUMP, s.segjump)
                 m.write_float(Binder.ADDR_PROG_LIMI, s.minlim)
                 m.write_float(Binder.ADDR_PROG_LIMA, s.maxlim)
                 if segtype == Binder.P_TEMP:
                     m.write_single_register(Binder.ADDR_PROG_OPERC, s.operc)
                 binder.reset()
         m.write_single_register(Binder.ADDR_PROG_END, 0)
-                    
-                
-            
